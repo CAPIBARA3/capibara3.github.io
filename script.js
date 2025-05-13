7f2f89db722232b86bd07e398e7d663d77feac38
joanalnu <joanalnu@outlook.com>
2025-05-13 11:08:03 +0200

    update menu bar styling and overall styles for the website

--- script.js
+++ script.js
@@ -8,25 +8,6 @@ function initBurger() {
 }
 initBurger();
 
-function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-}
-
-// Modal features
-// function openModal(modalId) {
-//     document.getElementById(modalId).style.display = 'block';
-// }
-// function closeModal(modalId) {
-//     document.getElementById(modalId).style.display = 'none';
-// }
-// window.onclick = function(event) {
-//     const modals = document.getElementsByClassName('modal');
-//     for (let modal of modals) {
-//         if (event.target == modal) {
-//             modal.style.display = 'none';
-//         }
-//     }
-// }
 
 // Burger menu toggle
 // const burger = document.getElementById('burger');
